2fd28f8cb68bc12500942c36b7cd918917eebaa3
DODO <thsehgml37@korea.ac.kr>
2021-08-22 04:58:41 +0900

    Feat : Update landing page images

--- src/components/carousel/MainCarousel.js
+++ src/components/carousel/MainCarousel.js
@@ -4,8 +4,11 @@ import "./MainCarousel.scss";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SlideImg1 from "./slide1.png";
-import SlideImg2 from "./slide2.jpg";
+import SlideImg1 from "./s5.png";
+import SlideImg2 from "./s2.png";
+import SlideImg3 from "./s3.png";
+import SlideImg4 from "./s4.png";
+
 // import SlideImg3 from "./painter.jpg";
 // import SlideImg4 from "./brush.jpg";
 
@@ -14,7 +17,7 @@ export default class SimpleSlider extends Component {
     const settings = {
       dots: false,
       infinite: true,
-      speed: 70,
+      speed: 30,
       autoplay: true,
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -41,15 +44,15 @@ export default class SimpleSlider extends Component {
             {/* <h3>1</h3> */}
           </div>
           <div>
-            <img src={SlideImg2} alt="slide img" />
+            <img src={SlideImg3} alt="slide img" />
             {/* <h3>2</h3> */}
           </div>
           <div>
-            <img src={SlideImg1} alt="slide img" />
+            <img src={SlideImg4} alt="slide img" />
             {/* <h3>3</h3> */}
           </div>
           <div>
-            <img src={SlideImg1} alt="slide img" />
+            <img src={SlideImg2} alt="slide img" />
             {/* <h3>4</h3> */}
           </div>
         </Slider>
