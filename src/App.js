c1529b4f418fb908da4017f5927b58d9bdb56688
DODO <thsehgml37@korea.ac.kr>
2021-08-21 21:29:28 +0900

    [UPDATE] default carousel added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import GlobalStyle from "./GlobalStyle";
+import MainPage from "./views/post/MainPage";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <MainPage />
       <div>hello world!</div>
     </>
   );
