c56b992a462a6b84a37c05e895223837a881d1c9
seol <suljiwon@naver.com>
2021-08-22 12:37:18 +0900

    Fix: url to deploy port

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-    baseURL:"http://127.0.0.1:8000/api",
+    baseURL:"http://115.85.183.95:7002/api",
     // withCredentials: true,
 });
 
