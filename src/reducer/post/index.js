5aad4bd682f0059362f0c118f88f2267e080bf05
DODO <thsehgml37@korea.ac.kr>
2022-04-14 23:56:17 +0900

    Fix: 예비 reducer 코드 작성 완료

--- src/reducer/post/index.js
+++ src/reducer/post/index.js
@@ -1,9 +1,42 @@
+//1차 준비 완료
+
+// //import {getPostDetailById} from '../../axios/Post';
 // const initialState = {
-//     id
-// }
+//     //id
+// };
 
-const postReducer = () => {
+// export const SET_POSTDETAIL = 'post/setPostDetail';
 
-};
+// // action creator
+// export const setPostDetail = (postDetail) =>({
+//     type:SET_POSTDETAIL,
+//     payload:postDetail,
+// });
 
-export default postReducer();
+// export const fetchPostDetail = (postId: String, accesstoken: String)=>{
+//     return async (dispatch) =>{
+//         try{
+//             const res = await getPostDetail(postId, accesstoken);
+//             dispatch(setPostDetail(res.data.data));
+//             console.log(res.data.message);
+//         } catch (err){
+//             console.error(err);
+//             console.log(err.response.data.message);
+//         }
+//     };
+// };
+
+// // Reducer
+// const reducer = (state = initialState, action) => {
+//     switch (action.type){
+//         case SET_POSTDETAIL: {
+//             return {
+//                 ...state,
+//                 detail: action.payload,
+//             };
+//         }
+//         default:
+//             return state;
+//     }
+// };
+// export default reducer;
