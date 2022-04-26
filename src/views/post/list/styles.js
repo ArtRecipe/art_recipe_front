fd757bcb7d7dd1a4d743ab34593080a0bf6680eb
DODO <thsehgml37@korea.ac.kr>
2022-04-26 23:32:22 +0900

    Fix: Infinite scroll

--- src/views/post/list/styles.js
+++ src/views/post/list/styles.js
@@ -6,3 +6,12 @@ export const PostListWrap = styled.div`
   flex-wrap: wrap;
   justify-content: space-around;
 `;
+
+export const LoaderWrap = styled.div`
+  width: 100%;
+  height: 80%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
