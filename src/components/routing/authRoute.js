8b77691f25024170e1c4e55e066cbb6891d4384c
DODO <thsehgml37@korea.ac.kr>
2022-05-15 19:27:49 +0900

    Fix: error fixed

--- src/components/routing/authRoute.js
+++ src/components/routing/authRoute.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Login from "../../views/user/AuthPage";
+import Login from "../../views/user/authPage/index";
 
 const AuthRoute = () => {
   const accesstoken = useSelector((state) => state.user.accesstoken);
   console.log("accesstoken 발급중....");
-  accesstoken ? console.log("access토큰 발급 성공") : console.log("accesstoken발급 안받음");
+  accesstoken
+    ? console.log("access토큰 발급 성공")
+    : console.log("accesstoken발급 안받음");
   return accesstoken ? <Outlet /> : <Navigate to="/auth" element={Login} />;
 };
 
