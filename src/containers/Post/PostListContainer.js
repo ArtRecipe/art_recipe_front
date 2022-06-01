b690c9ab04bdf6f24041a8f531228182f94a73a2
seol <suljiwon@naver.com>
2022-06-02 00:02:36 +0900

    Refactor: refactor post list, implement post list container

---
+++ src/containers/Post/PostListContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./postList.module.scss";
+
+import { PostBanner } from "../../components/Banner/PostBanner";
+import { PostCard } from "../../components/Post/PostCard";
+
+export const PostListContainer = ({ data }) => {
+  if (data) {
+    return (
+      <>
+        <PostBanner />
+        <div className={styles.postListWrap}>
+          {data.map((post) => {
+            return <PostCard key={post.id} post={post} />;
+          })}
+          ;
+        </div>
+      </>
+    );
+  }
+  return null;
+};
+
+PostListContainer.propTypes = {
+  data: PropTypes.array,
+};
+
+export default PostListContainer;
